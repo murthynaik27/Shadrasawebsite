937511ce39de8fbe126fdc026546fb930a0b765f
murthy <murtinaik21@gmail.com>
2026-05-21 21:51:29 +0530

    fix: update react router basename for vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ import "./App.css";
 function App() {
   return (
     <CartProvider>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename="/">
         <Toaster position="top-right" richColors closeButton />
         <CartDrawer />
         <Routes>
